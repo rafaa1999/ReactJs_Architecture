de437dc55c0cf537bc434ab184c55199871596f4
rafaa1999 <raferhouma10@gmail.com>
2023-05-08 19:58:31 +0100

    setup react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import "bootstrap/dist/css/bootstrap.min.css"
+import NewProduct from "./components/NewProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <nav>
+      <ul className="nav nav-pills p-1 m-3">
+      <li>
+            <Link className="btn btn-outline-info ms-1" to={"/"}>
+              Home
+            </Link>
+            <Link className="btn btn-outline-info ms-1" to={"/products"}>
+              Products
+            </Link>
+            <Link className="btn btn-outline-info ms-1" to={"/newProduct"}>
+              New Product
+            </Link>
+          </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/Products" element={<Products/>} />
+      <Route path="/NewProduct" element={<NewProduct/>} />
+    </Routes>
+   </BrowserRouter>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div>
+      <h1>product</h1>
+      <p>welcome to home</p>
+    </div>
+  )
+}
+
+export default Products
